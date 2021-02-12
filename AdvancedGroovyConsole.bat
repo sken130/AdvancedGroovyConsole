f76fe96d5ff3c25ca3b301fb8703466c9623c92d
Ken <sken130@gmail.com>
2021-02-12 21:32:54 +0800

    More sample codes for AdvancedGroovyConsole.bat to clear screen, change Java version, or change Groovy version.

--- AdvancedGroovyConsole.bat
+++ AdvancedGroovyConsole.bat
@@ -1,3 +1,15 @@
+REM cls
+
+REM set OLD_JAVA_HOME=%JAVA_HOME%
+REM set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_172
+REM set JAVA_HOME=C:\Program Files\AdoptOpenJDK\jdk-15.0.2.7-hotspot
+REM call SET PATH=%%PATH:%OLD_JAVA_HOME%=%JAVA_HOME%%%
+
+REM set OLD_GROOVY_HOME=%GROOVY_HOME%
+REM set GROOVY_HOME=C:\groovy\groovy-2.4.15
+REM set GROOVY_HOME=C:\groovy\groovy-3.0.7
+REM call SET PATH=%%PATH:%OLD_GROOVY_HOME%=%GROOVY_HOME%%%
+
 set CLASSPATH=src/;lib/*
 
 groovy src/com/kenlam/groovyconsole/AdvancedGroovyConsole.groovy
