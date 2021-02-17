2031afc0dd1c8e81910793f17437482655a4f199
Ken <sken130@gmail.com>
2021-02-17 11:11:10 +0800

    1. Update AdvancedGroovyConsole.bat to use gradlew run 2. Upgrade to Java 11 and Groovy 3.0.7 (if not already upgraded) 3. Fix compilation errors in Java 11

--- AdvancedGroovyConsole.bat
+++ AdvancedGroovyConsole.bat
@@ -8,13 +8,12 @@ call SET PATH=%%PATH:%OLD_JAVA_HOME%=%JAVA_HOME%%%
 
 set OLD_GROOVY_HOME=%GROOVY_HOME%
 REM set GROOVY_HOME=C:\groovy\groovy-2.4.15
-set GROOVY_HOME=C:\groovy\groovy-2.5.14
-REM set GROOVY_HOME=C:\groovy\groovy-3.0.7
+REM set GROOVY_HOME=C:\groovy\groovy-2.5.14
+set GROOVY_HOME=C:\groovy\groovy-3.0.7
 call SET PATH=%%PATH:%OLD_GROOVY_HOME%=%GROOVY_HOME%%%
 
-set CLASSPATH=src/;lib/*
-
-REM set JAVA_OPTS=--add-modules java.xml.bind
 REM set JAVA_OPTS=
 
-groovy src/com/kenlam/groovyconsole/AdvancedGroovyConsole.groovy
+REM groovy src/com/kenlam/groovyconsole/AdvancedGroovyConsole.groovy
+
+gradlew run
